d8791f2bb00a36795a9180b0b6db05158d86601b
xiaolinhua <xiaolinhua@baijiahulian.com>
2020-11-20 14:17:43 +0800

    java面试

---
+++ codeTest/src/main/java/concurrent/CountDownLatchExample.java
@@ -0,0 +1,52 @@
+package concurrent;
+
+/**
+ * @author linhua
+ * @apiNote
+ * @date 2020-03-24 12:08 下午
+ */
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CountDownLatchExample {
+
+    private ExecutorService executorService;
+    private CountDownLatch countDownLatch;
+    private int parties;
+
+    public static void main(String[] args){
+        CountDownLatchExample countDownLatchExample = new CountDownLatchExample(10);
+        try {
+            countDownLatchExample.example();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public CountDownLatchExample(int parties) {
+        executorService = Executors.newFixedThreadPool(parties);
+        countDownLatch = new CountDownLatch(parties);
+        this.parties = parties;
+    }
+
+    public void example() throws InterruptedException {
+        for (int i = 0; i < parties; i++) {
+            executorService.submit(() -> {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + " gets job done");
+                countDownLatch.countDown(); // 线程完成任务之后countDown
+            });
+        }
+        // 等待所有的任务完成
+        countDownLatch.await();
+        System.out.println(Thread.currentThread().getName() + " reach barrier");
+        executorService.shutdown();
+    }
+}
+
---
+++ codeTest/src/main/java/concurrent/CyclicBarrierExample.java
@@ -0,0 +1,48 @@
+package concurrent;
+
+/**
+ * @author linhua
+ * @apiNote
+ * @date 2020-03-25 10:42 上午
+ */
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CyclicBarrierExample {
+
+    private ExecutorService executorService;
+    private CyclicBarrier cyclicBarrier;
+    private int parties;
+
+    public CyclicBarrierExample(int parties) {
+        executorService = Executors.newFixedThreadPool(parties);
+        cyclicBarrier = new CyclicBarrier(parties, () -> System.out.println(Thread.currentThread().getName() + " gets barrierCommand done"));
+        this.parties = parties;
+    }
+
+    public static void main(String[] args) {
+        CyclicBarrierExample cyclicBarrierExample = new CyclicBarrierExample(10);
+        cyclicBarrierExample.example();
+    }
+
+    public void example() {
+        for (int i = 0; i < parties; i++) {
+            executorService.submit(() -> {
+                try {
+                    Thread.sleep(1000);
+                    cyclicBarrier.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + " gets job done");
+            });
+        }
+        executorService.shutdown();
+    }
+}
+
